75fda9118498b10d258220389a644b845778b34b
Ace Assured <127981608+aceassured@users.noreply.github.com>
2023-11-16 18:26:57 +0530

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -2,6 +2,14 @@
 
 		#Run migrations to ensure the database is updated
 		npx medusa migrations run && npx medusa start
+
+		#Admin username & password
+                railway run npx medusa user --email bharath@aceassured.com --password aceassured
+
+                  
 		
 		#Start development environment
 		#npm run start 
+  
+
+  
