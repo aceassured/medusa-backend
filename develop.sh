43b544e71848aa0d758e4d1ca5203cbc7989a7d5
kishorevenai <kishorevenai@gmail.com>
2023-11-20 17:29:21 +0530

    09

--- develop.sh
+++ develop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 		#Run migrations to ensure the database is updated
-		npx medusa migrations && npx medusa user --email bharath@aceassured.com --password aceassured run && npx medusa start
+		npx medusa migrations && npx medusa user --email bharath@aceassured.com [--password aceassured] run && npx medusa start
 
 		#Admin username & password
                 
